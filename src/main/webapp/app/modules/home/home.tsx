5d9286659da016958f4847dad241ffb3e5b6daf0
Samuel Cunha <samuelcunha@vipcommerce.com.br>
2021-06-20 22:36:58 -0300

    Criando home para usuário não autenticado

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,100 +1,53 @@
 import './home.scss';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
+import { Button, Container, Grid, makeStyles } from '@material-ui/core';
 
 export type IHomeProp = StateProps;
 
-export const Home = (props: IHomeProp) => {
-  const { account } = props;
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: theme.spacing(10),
+  },
+  logo: {
+    display: 'flex',
+    margin: 'auto',
+    maxWidth: '350px',
+    width: '100%',
+    marginBottom: theme.spacing(10),
+  },
+  actions: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+  },
+  button: {
+    minWidth: '200px',
+    margin: 'auto',
+    marginBottom: theme.spacing(1),
+  },
+}));
 
+export const Home = () => {
+  const classes = useStyles();
   return (
-    <Row>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
-      <Col md="9">
-        <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
-        </p>
-        {account && account.login ? (
-          <div>
-            <Alert color="success">
-              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
-        <p>
-          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-        </p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.follow">follow @jhipster on Twitter</Translate>
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          !
-        </p>
-      </Col>
-    </Row>
+    <Container disableGutters maxWidth={false} className={classes.container}>
+      <Grid container justify="center">
+        <Grid item xs={12}>
+          <img src="content/images/logo.png" alt="Logo" className={classes.logo} />
+        </Grid>
+        <Grid item xs={12} className={classes.actions}>
+          <Button variant="contained" color="primary" disableElevation component={Link} to="/login" className={classes.button}>
+            Login
+          </Button>
+          <Button variant="contained" color="primary" disableElevation component={Link} to="/account/register" className={classes.button}>
+            Registrar
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
