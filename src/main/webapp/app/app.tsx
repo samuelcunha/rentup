e7130eeebe640d68db53a878c9523c8fe1d39f16
Samuel Cunha <samuelcunha@vipcommerce.com.br>
2021-06-20 22:31:36 -0300

    Adicionado MaterialUI

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -4,7 +4,6 @@ import 'app/config/dayjs.ts';
 
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Card } from 'reactstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { hot } from 'react-hot-loader';
@@ -13,64 +12,41 @@ import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
 import { getProfile } from 'app/shared/reducers/application-profile';
 import { setLocale } from 'app/shared/reducers/locale';
-import Header from 'app/shared/layout/header/header';
-import Footer from 'app/shared/layout/footer/footer';
-import { hasAnyAuthority } from 'app/shared/auth/private-route';
 import ErrorBoundary from 'app/shared/error/error-boundary';
-import { AUTHORITIES } from 'app/config/constants';
 import AppRoutes from 'app/routes';
+import { Container, CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import theme from './config/theme';
 
 const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
 
-export interface IAppProps extends StateProps, DispatchProps {}
-
-export const App = (props: IAppProps) => {
+export const App = props => {
   useEffect(() => {
     props.getSession();
     props.getProfile();
   }, []);
 
-  const paddingTop = '60px';
   return (
-    <Router basename={baseHref}>
-      <div className="app-container" style={{ paddingTop }}>
-        <ToastContainer position={toast.POSITION.TOP_LEFT} className="toastify-container" toastClassName="toastify-toast" />
-        <ErrorBoundary>
-          <Header
-            isAuthenticated={props.isAuthenticated}
-            isAdmin={props.isAdmin}
-            currentLocale={props.currentLocale}
-            onLocaleChange={props.setLocale}
-            ribbonEnv={props.ribbonEnv}
-            isInProduction={props.isInProduction}
-            isOpenAPIEnabled={props.isOpenAPIEnabled}
-          />
-        </ErrorBoundary>
-        <div className="container-fluid view-container" id="app-view-container">
-          <Card className="jh-card">
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router basename={baseHref}>
+        <div className="app-container">
+          <ToastContainer position={toast.POSITION.TOP_LEFT} className="toastify-container" toastClassName="toastify-toast" />
+          <Container disableGutters maxWidth={false}>
             <ErrorBoundary>
               <AppRoutes />
             </ErrorBoundary>
-          </Card>
-          <Footer />
+          </Container>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </MuiThemeProvider>
   );
 };
 
-const mapStateToProps = ({ authentication, applicationProfile, locale }: IRootState) => ({
+const mapStateToProps = ({ authentication, locale }: IRootState) => ({
   currentLocale: locale.currentLocale,
   isAuthenticated: authentication.isAuthenticated,
-  isAdmin: hasAnyAuthority(authentication.account.authorities, [AUTHORITIES.ADMIN]),
-  ribbonEnv: applicationProfile.ribbonEnv,
-  isInProduction: applicationProfile.inProduction,
-  isOpenAPIEnabled: applicationProfile.isOpenAPIEnabled,
 });
 
 const mapDispatchToProps = { setLocale, getSession, getProfile };
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
 
 export default connect(mapStateToProps, mapDispatchToProps)(hot(module)(App));
---
+++ src/main/webapp/app/config/theme.tsx
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3445A3',
+    },
+
+    secondary: {
+      main: '#0F7B1B',
+    },
+  },
+});
+
+export default theme;
