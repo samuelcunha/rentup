54ae8a2029a912bf7df7800080e421e4045fe0bf
Samuel Cunha <samuelcunha@vipcommerce.com.br>
2021-06-21 22:14:39 -0300

    Adicionando drawer ao header

--- src/main/webapp/app/modules/login/logout.tsx
+++ src/main/webapp/app/modules/login/logout.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { IRootState } from 'app/shared/reducers';
 import { logout } from 'app/shared/reducers/authentication';
+import { Redirect } from 'react-router-dom';
 
 export interface ILogoutProps extends StateProps, DispatchProps {
   idToken: string;
@@ -21,11 +22,7 @@ export const Logout = (props: ILogoutProps) => {
     }
   });
 
-  return (
-    <div className="p-5">
-      <h4>Logged out successfully!</h4>
-    </div>
-  );
+  return <Redirect to="/" />;
 };
 
 const mapStateToProps = (storeState: IRootState) => ({
--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -1,12 +1,30 @@
 import './header.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import LoadingBar from 'react-redux-loading-bar';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-import { AppBar, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Avatar,
+  Card,
+  CardHeader,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,10 +40,46 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  drawer: {
+    minWidth: 250,
+  },
 }));
 
 const Header = props => {
   const classes = useStyles();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const renderDrawer = () => (
+    <div className={classes.drawer} onClick={() => setMenuOpen(!menuOpen)} onKeyDown={() => setMenuOpen(!menuOpen)}>
+      <Link to="/account/settings" style={{ textDecoration: 'none' }}>
+        <Card variant="outlined">
+          <CardHeader avatar={<Avatar></Avatar>} title={props.account.firstName} subheader={props.account.email} />
+        </Card>
+      </Link>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inicio" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <StorefrontIcon />
+          </ListItemIcon>
+          <ListItemText primary="Meus Anúncios" />
+        </ListItem>
+        <Link to="/logout">
+          <ListItem button>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sair" />
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
@@ -33,7 +87,7 @@ const Header = props => {
         <LoadingBar className="loading-bar" />
         <Toolbar>
           {props.isAuthenticated && (
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <IconButton edge="start" className={classes.menuButton} color="inherit" onClick={() => setMenuOpen(true)}>
               <MenuIcon />
             </IconButton>
           )}
@@ -42,6 +96,9 @@ const Header = props => {
             {props.title}
           </Typography>
         </Toolbar>
+        <Drawer anchor="left" open={menuOpen} onClose={() => setMenuOpen(false)}>
+          {renderDrawer()}
+        </Drawer>
       </AppBar>
     </div>
   );
